5993216eb89e9845085c6d5af475463bdb6ccb2e
Camtendo <ccrockrom@gmail.com>
2019-05-18 14:47:33 -0500

    Add OBS WebSocket support

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,9 @@
-$('button').click(serverRequests);
+$('#update-btn').click(serverRequests);
+$('#stream-btn').click(toggleOBSStreaming);
+let isStreaming = false;
+
+getOBSStatus();
+setInterval(getOBSStatus, 500);
 
 function serverRequests() {
     var playerOne = $('#playerOneInput').val();
@@ -9,7 +14,7 @@ function serverRequests() {
         return;
     }
 
-    disableButtonTemporarily();
+    disableButtonsTemporarily();
     $.get(`/update-twitch/${playerOne}/${playerTwo}`).done(() => {
         $.get(`/notify-slack/${playerOne}/${playerTwo}`).done(() => {
             updateMarquee(playerOne, playerTwo);
@@ -21,14 +26,48 @@ function serverRequests() {
     });
 }
 
+function getOBSStatus() {
+    $.get(`/obs/streaming-status`).done((result) => {
+        if (result.streaming != isStreaming) {
+            console.log(`Updated streaming status to ${result.streaming}`);
+            updateStreamingButton(result.streaming);
+        }
+        isStreaming = result.streaming;
+    }).fail(() => {
+        console.log('Unable to request OBS status');
+    });
+}
+
+function toggleOBSStreaming() {
+    disableButtonsTemporarily();
+    $.get(`/obs/set-streaming/${!isStreaming}`).done((result) => {
+        updateStreamingButton(isStreaming);
+    }).fail(() => {
+        console.log('Unable to toggle OBS streaming');
+    });
+}
+
 function updateMarquee(playerOne, playerTwo) {
     var newString = `${playerOne} vs. ${playerTwo}`;
     $('marquee').text(newString);
 }
 
-function disableButtonTemporarily() {
+function updateStreamingButton(currentlyStreaming) {
+    var streamButton = $('#stream-btn');
+    if (currentlyStreaming) {
+        streamButton.removeClass('btn-secondary');
+        streamButton.addClass('btn-danger');
+        streamButton.text('Stop Streaming');
+    } else {
+        streamButton.removeClass('btn-danger');
+        streamButton.addClass('btn-secondary');
+        streamButton.text('Start Streaming');
+    }
+}
+
+function disableButtonsTemporarily() {
     $('button').prop('disabled', true);
     setTimeout(function() {
         $('button').prop('disabled', false);
-    }, 10000);
+    }, 2000);
 }
--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 var path = require('path');
 const TwitchClient = require('twitch').default;
 var SlackBot = require('slackbots');
+var OBSWebSocket = require('obs-websocket-js');
 var config = require('./config.json');
 
 const app = express();
 const slackBot = new SlackBot({
-    token: config.slackToken,
-    name: config.slackBotName
+  token: config.slackToken,
+  name: config.slackBotName
 });
+const obs = new OBSWebSocket();
 
 let twitchClient;
 (async () => {
-    twitchClient = await TwitchClient.withCredentials(config.twitchClientId, config.twitchOAuthToken);
+  twitchClient = await TwitchClient.withCredentials(config.twitchClientId, config.twitchOAuthToken);
 })();
 
 
@@ -20,34 +22,64 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(`Starting ez-vs-streamer on port ${port}...`);
-console.log(config);
+console.log(`Using config with ${config} keys`);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.get('/update-twitch/:player1/:player2', async (req, res) => {
-    const newTitle = `${req.params.player1} vs. ${req.params.player2}`;
-    const channel = await twitchClient.kraken.channels.getMyChannel();
-    twitchClient.kraken.channels.updateChannel(channel, {status: newTitle});
+  const newTitle = `${req.params.player1} vs. ${req.params.player2}`;
+  const channel = await twitchClient.kraken.channels.getMyChannel();
+  twitchClient.kraken.channels.updateChannel(channel, { status: newTitle });
 
-    console.log('Twitch channel successfully updated');
-    res.sendStatus(200);
+  console.log('Twitch channel successfully updated');
+  res.sendStatus(200);
 });
 
 app.get('/notify-slack/:player1/:player2', (req, res) => {
-    var message = `A new match is about to begin! ${req.params.player1} vs. ${req.params.player2}`;
-    var urlMessage = `Watch it on ${config.twitchChannelUrl}`;
+  var message = `A new match is about to begin! ${req.params.player1} vs. ${req.params.player2}`;
+  var urlMessage = `Watch it on ${config.twitchChannelUrl}`;
 
-    var params = {icon_emoji:':finalsmash:'};
-    if (config.isDebug) {
-        slackBot.postMessageToUser(config.slackTestUsername, message, params, () => {
-            slackBot.postMessageToUser(config.slackTestUsername, urlMessage, params);
-        });
-    } else {
-        slackBot.postMessageToChannel(config.slackRoomName, message, params, () => {
-            slackBot.postMessageToChannel(config.slackRoomName, urlMessage, params);
-        });
-    }
-    res.sendStatus(200);
+  var params = { icon_emoji: ':finalsmash:' };
+  if (config.isDebug) {
+    slackBot.postMessageToUser(config.slackTestUsername, message, params, () => {
+      slackBot.postMessageToUser(config.slackTestUsername, urlMessage, params);
+    });
+  } else {
+    slackBot.postMessageToChannel(config.slackRoomName, message, params, () => {
+      slackBot.postMessageToChannel(config.slackRoomName, urlMessage, params);
+    });
+  }
+  res.sendStatus(200);
+});
+
+obs.connect()
+  .then(() => {
+    console.log('Connected to OBS successfully');
+    return obs.send('GetVersion');
+  });
+
+obs.on('error', err => {
+  console.error('socket error:', err);
+});
+
+app.get('/obs/streaming-status', async (req, res) => {
+  var status = await obs.send('GetStreamingStatus');
+  res.json(status);
+});
+
+app.get('/obs/set-streaming/:shouldStream', async (req, res) => {
+  const shouldStream = req.params.shouldStream;
+  let streaming = false;
+
+  if (shouldStream === 'true') {
+    console.log('Attempting to start streaming...');
+    streaming = await obs.send('StartStreaming')
+  } else {
+    console.log('Attempting to stop streaming...');
+    streaming = await obs.send('StopStreaming');
+  }
+
+  res.json(streaming);
 });
 
 app.listen(port);
